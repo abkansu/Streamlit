6a24f3b6cdfaeaab031a48089d7a51ca3ecd8865
Ahmet Bera kansu <abkansu@gmail.com>
2023-01-06 18:58:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def print_hi():
+    st.text("This is a streamlit application")
+    st.markdown("""---""")
+    st.text("Created by:\nAhmet Bera Kansu - Emirhan Topcu")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
